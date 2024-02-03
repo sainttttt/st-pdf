90d19d0af9d622eff98fa0b3858ba32456fed798
saint <sainttttt@x265.ca>
2024-02-03 16:22:51 -0500

    init commit

---
+++ gen.nim
@@ -0,0 +1,38 @@
+import asynchttpserver, asyncdispatch
+import nimja/parser
+import os, random # os and random are later used in the templates, so imported here
+
+type
+  User = object
+    name: string
+    lastname: string
+    age: int
+
+proc renderIndex(title: string, users: seq[User]): string =
+  ## the `index.nimja` template is transformed to nim code.
+  ## so it can access all variables like `title` and `users`
+  ## the return variable could be `string` or `Rope` or
+  ## anything which has a `&=`(obj: YourObj, str: string) proc.
+  compileTemplateFile(getScriptDir() / "index.nimja")
+
+proc main {.async.} =
+  var server = newAsyncHttpServer()
+
+  proc cb(req: Request) {.async.} =
+
+    # in the templates we can later loop trough this sequence
+    let users: seq[User] = @[
+      User(name: "Katja", lastname: "Kopylevych", age: 32),
+      User(name: "David", lastname: "Krause", age: 32),
+    ]
+    await req.respond(Http200, renderIndex("index", users))
+
+  server.listen Port(8080)
+  while true:
+    if server.shouldAcceptRequest():
+      await server.acceptRequest(cb)
+    else:
+      poll()
+
+asyncCheck main()
+runForever()
